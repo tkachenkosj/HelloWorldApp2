f692e68909bb4971de6937c05e59046abbb7189f
Sergey Tkachenko <tkachenkosj@gmail.com>
2020-09-16 14:59:46 +0300

    Initial Commit

---
+++ HelloWorldApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorldApp2
+//
+//  Created by Сергей Ткаченко on 16.09.2020.
+//  Copyright © 2020 Sergey Tkachenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
